b44c6c43a9b138b8162ad8d6c0f88ad81171c8be
Moncef Belyamani <giter@done>
2013-10-09 11:24:07 -0700

    Add DB status checks and dependencies

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,10 +2,21 @@ class StatusController < ApplicationController
   respond_to :json
 
   def get_status
+    # DB checks
+    test_location = Location.first
+    test_category = Category.first
+
+    if test_location.present? && test_category.present?
+      status = "ok"
+    else
+      status = "DB returned blank location or category"
+    end
+
     render json:
       {
-        "status" => "ok",
+        "status" => status,
         "updated" => Time.now.to_i
+        "dependencies" => ["Mandrill","Mongolab","Redis To Go","MemCachier","Elasticsearch"]
       }
   end
 end
