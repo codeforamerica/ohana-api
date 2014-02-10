ff5edf5b79b727460282472968f2ea9ef51e0bb9
Moncef Belyamani <giter@done>
2014-02-10 00:07:51 -0500

    Add slug to Category
    This will allow categories to be updated from the admin interface via their slugs as opposed to their DB IDs, which are not guaranteed to always be the same, especially when testing both the API and admin interface locally. The specs for the admin interface currently communicate with an instance of the API running locally on the same machine as the admin interface.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category
   include Mongoid::Document
+  include Mongoid::Slug
   include Grape::Entity::DSL
   acts_as_nested_set
+
+  slug :name, history: true
 
   has_and_belongs_to_many :services
   #has_many :services
@@ -12,5 +15,6 @@ class Category
     expose :oe_id
     expose :name
     expose :parent_id
+    expose :slugs
   end
 end
