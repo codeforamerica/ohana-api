a317a096b8cef2257323472eddf12543eb73ff5f
Moncef Belyamani <giter@done>
2014-06-10 13:25:41 -0400

    Allow users to sign up on developer portal.
    See issue 164 in upstream repo: https://github.com/codeforamerica/ohana-api/issues/164

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ OhanaApi::Application.configure do
   config.action_mailer.smtp_settings = {
     port:           '587',
     address:        'smtp.mandrillapp.com',
-    user_nam:       ENV['MANDRILL_USERNAME'],
+    user_name:       ENV['MANDRILL_USERNAME'],
     password:       ENV['MANDRILL_APIKEY'],
     domain:         'heroku.com',
     authentication: :plain
