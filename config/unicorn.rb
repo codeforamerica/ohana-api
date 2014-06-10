310a5e30f489b30e6cc013757dc2db85b4b6fd0c
Moncef Belyamani <giter@done>
2014-06-10 14:38:22 -0400

    Fix unicorn setup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
 
-before_fork do
+before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -19,7 +19,7 @@ before_fork do
   end
 end
 
-after_fork do
+after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
