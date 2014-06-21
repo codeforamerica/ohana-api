9ae091d67b725ef4930d21d60c5658dce936b4d7
Moncef Belyamani <giter@done>
2014-06-20 23:27:18 -0400

    Try tld of 2 for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 OhanaApi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_dispatch.tld_length = 2
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
