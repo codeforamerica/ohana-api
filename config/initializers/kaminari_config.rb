a09bd3900dafeec28c705607c5bcc5b243713e33
Moncef Belyamani <giter@done>
2014-06-27 10:29:21 -0400

    Update documentation

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,5 @@
 # See Kaminari README for more details about config options:
 # https://github.com/amatsuda/kaminari#general-configuration-options
-# The options below use settings defined in config/settings.yml.
 Kaminari.configure do |config|
   config.default_per_page = ENV['DEFAULT_PER_PAGE'].to_i
   config.max_per_page = ENV['MAX_PER_PAGE'].to_i
