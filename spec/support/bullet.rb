476356605d1ef424f2a429c85411f1a18089e536
Moncef Belyamani <giter@done>
2014-07-03 23:26:47 -0400

    Update bullet to version 4.11.3

--- spec/support/bullet.rb
+++ spec/support/bullet.rb
@@ -1,10 +1,13 @@
 RSpec.configure do |config|
 
-  config.before(:each) do
-    Bullet.start_request if Bullet.enable?
-  end
+  if Bullet.enable?
+    config.before(:each) do
+      Bullet.start_request
+    end
 
-  config.after(:each) do
-    Bullet.end_request if Bullet.enable?
+    config.after(:each) do
+      # Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
   end
 end
