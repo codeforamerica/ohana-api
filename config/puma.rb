032bed49b00fd516ae8959cdb88edea3920bbcea
Moncef Belyamani <giter@done>
2014-09-07 02:01:03 -0400

    Try clustered mode.

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,18 @@
 threads 0, 16
-# workers 2
-# preload_app!
+workers 2
+preload_app!
 
-# on_restart do
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.connection.disconnect!
-#     Rails.logger.info('Disconnected from ActiveRecord')
-#   end
+on_restart do
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
 
-#   if defined?(Redis)
-#     REDIS.quit
-#     Rails.logger.info('Disconnected from Redis')
-#   end
-# end
+  # if defined?(Redis)
+  #   REDIS.quit
+  #   Rails.logger.info('Disconnected from Redis')
+  # end
+end
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
