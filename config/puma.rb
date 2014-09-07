78e9569c81ab5d68d123d61897261a6b30ebd851
Moncef Belyamani <giter@done>
2014-09-07 02:35:46 -0400

    Use Heroku-recommended settings for Puma.
    https://devcenter.heroku.com/articles/deploying-rails-applications-with-
    the-puma-web-server
    
    https://devcenter.heroku.com/articles/concurrency-and-database-connectio
    ns#threaded-servers

--- config/puma.rb
+++ config/puma.rb
@@ -1,27 +1,24 @@
-threads 0, 16
-workers 2
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+
 preload_app!
 
-on_restart do
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
-  end
-
-  # if defined?(Redis)
-  #   REDIS.quit
-  #   Rails.logger.info('Disconnected from Redis')
-  # end
-end
+rackup DefaultRackup
+port ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
+  # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
     Rails.logger.info('Connected to ActiveRecord')
   end
 
-  # if defined?(Redis)
-  #   REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
-  #   Rails.logger.info('Connected to Redis')
-  # end
+  if defined?(Redis)
+    REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+    Rails.logger.info('Connected to Redis')
+  end
 end
