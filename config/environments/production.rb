a80e079ad2129a38035765d9d03fdf92f30cfea4
Moncef Belyamani <giter@done>
2014-09-28 23:23:50 -0400

    Use Heroku settings for Memcachier.
    https://devcenter.heroku.com/articles/memcachier#rails-3-and-4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store,
+                       (ENV['MEMCACHIER_SERVERS'] || '').split(','),
+                       {
+                         username: ENV['MEMCACHIER_USERNAME'],
+                         password: ENV['MEMCACHIER_PASSWORD'],
+                         failover: true,
+                         socket_timeout: 1.5,
+                         socket_failure_delay: 0.2
+                       }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
