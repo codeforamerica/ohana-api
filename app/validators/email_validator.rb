d22e2b5e08692b805eab33e3983fa5b160fcb386
Moncef Belyamani <giter@done>
2014-11-10 10:22:40 -0500

    Refactor regex validators.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,10 +1,9 @@
-class EmailValidator < ActiveModel::EachValidator
+class EmailValidator < RegexValidator
   def validate_each(record, attribute, value)
-    return if value.blank?
     default_message = "#{value} #{I18n.t('errors.messages.invalid_email')}"
 
-    unless value =~ /\A([^@\s]+)@((?:(?!-)[-a-z0-9]+(?<!-)\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || default_message)
-    end
+    regex =  /\A([^@\s]+)@((?:(?!-)[-a-z0-9]+(?<!-)\.)+[a-z]{2,})\z/i
+
+    regex_validate_each(regex, default_message, record, attribute, value)
   end
 end
