ea05f248ef129b62ca29920495df153b772b6a09
Moncef Belyamani <giter@done>
2014-11-30 12:50:17 -0500

    Add missing search params to root endpoint.

--- spec/api/root_endpoint_spec.rb
+++ spec/api/root_endpoint_spec.rb
@@ -6,10 +6,13 @@ describe 'API Root Endpoint' do
     hash = {
       'organizations_url' => "#{api_organizations_url}{?page,per_page}",
       'organization_url' => "#{api_organizations_url}/{organization}",
-      'organization_locations_url' => "#{api_url}/organizations/{organization}/locations{?page,per_page}",
+      'organization_locations_url' => "#{api_url}/organizations/{organization}"\
+        '/locations{?page,per_page}',
       'locations_url' => "#{api_locations_url}{?page,per_page}",
       'location_url' => "#{api_locations_url}/{location}",
-      'location_search_url' => "#{api_search_index_url}{?category,email,keyword,language,lat_lng,org_name,radius,service_area,status,page,per_page}"
+      'location_search_url' => "#{api_search_index_url}{?category,email,"\
+        'keyword,language,lat_lng,location,org_name,radius,service_area,'\
+        'status,page,per_page}'
     }
     expect(json).to eq(hash)
   end
