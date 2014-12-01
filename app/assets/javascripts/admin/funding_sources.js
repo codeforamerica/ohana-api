04b7bfe7ddd09ece1fe7430151b7da5f6bb85315
Moncef Belyamani <giter@done>
2014-12-01 00:06:43 -0500

    Clean up duplication in funding_sources.js

--- app/assets/javascripts/admin/funding_sources.js
+++ app/assets/javascripts/admin/funding_sources.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#organization_funding_sources,#service_funding_sources,#organization_accreditations,#organization_funding_sources,#organization_licenses").select2({
+  $("#service_funding_sources,#organization_accreditations,#organization_funding_sources,#organization_licenses").select2({
     tags: [''],
     tokenSeparators: [',']
   });
