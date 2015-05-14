e1a66697d4a3955cf88bdafa5f429b02c3371448
Moncef Belyamani <giter@done>
2015-05-14 00:24:46 -0400

    Conditionally validate presence of state_province
    Not all countries have states or provinces, and not all countries that have them require them in postal addresses. For now, the app only requires them for the US and Canada.

--- app/validators/state_province_validator.rb
+++ app/validators/state_province_validator.rb
@@ -2,11 +2,10 @@ class StateProvinceValidator < ActiveModel::EachValidator
   COUNTRIES_NEEDING_VALIDATION = %w(US CA).freeze
 
   def validate_each(record, attribute, value)
-    return if value.blank?
     return unless COUNTRIES_NEEDING_VALIDATION.include?(record.country)
     default_message = I18n.t('errors.messages.invalid_state_province')
 
-    unless value.size == 2
+    unless value.present? && value.size == 2
       record.errors[attribute] << (options[:message] || default_message)
     end
   end
