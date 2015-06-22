12b6d40a9704d00bde5376b9cebc304794500a68
Moncef Belyamani <giter@done>
2015-06-21 23:34:30 -0400

    Update sample csv files
    Now that we have an export feature, I exported the demo data as CSV files and used that to replace the old sample CSV files, which were manually created with dummy data.
    
    I also fixed the accessibility CSV export so that it uses the format that's expected when importing the data.

--- spec/features/admin/csv/download_locations_csv_spec.rb
+++ spec/features/admin/csv/download_locations_csv_spec.rb
@@ -19,7 +19,7 @@ feature 'Downloading Locations CSV' do
     it 'converts arrays to comma-separated strings' do
       expect(csv.second).to eq [
         @loc.id.to_s, @loc.organization_id.to_s,
-        'Information on tape or in Braille, Disabled Parking',
+        'tape_braille, disabled_parking',
         'foo@bar.com', nil, 'Provides job training', 'bar@foo.com',
         'English, Vietnamese', '37.583939', '-122.3715745', 'VRS Services',
         'Provides job training.', 'BART stop 1 block away.',
