60d380863a996e5be47ffe1b5f3b5c9615e2f9d1
Moncef Belyamani <giter@done>
2015-11-16 12:51:32 -0500

    Update shoulda-matchers to version 3.0.1

---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,10 @@
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+  end
+end
