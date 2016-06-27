2783fd98160854c12c1a050648db8662fc4f02c3
Moncef Belyamani <monfresh@users.noreply.github.com>
2016-06-26 23:01:08 -0400

    Clean up CSS (#387)
    **Why**: To address scss-lint offenses

--- app/helpers/admin/form_helper.rb
+++ app/helpers/admin/form_helper.rb
@@ -6,13 +6,13 @@ class Admin
       fields = f.fields_for(association, new_object, child_index: id) do |builder|
         render("admin/locations/forms/#{association.to_s.singularize}_fields", f: builder)
       end
-      link_to(name, '#', class: 'add_fields btn btn-primary', data: { id: id, fields: fields.gsub('\n', '') })
+      link_to(name, '#', class: 'add-fields btn btn-primary', data: { id: id, fields: fields.gsub('\n', '') })
     end
 
     def link_to_add_array_fields(name, model, field)
       id = ''.object_id
       fields = render("admin/#{model}/forms/#{field}_fields")
-      link_to(name, '#', class: 'add_array_fields btn btn-primary', data: { id: id, fields: fields.gsub('\n', '') })
+      link_to(name, '#', class: 'add-array-fields btn btn-primary', data: { id: id, fields: fields.gsub('\n', '') })
     end
 
     def nested_categories(categories)
