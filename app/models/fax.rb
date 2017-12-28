f26cce9f3dbe9e188f54be3e110a91de853d7f27
Moncef Belyamani <giter@done>
2017-12-28 00:46:25 -0500

    Remove unused files
    The Faxes table was removed a long time ago.

--- app/models/fax.rb
+++
@@ -1,9 +0,0 @@
-class Fax < ActiveRecord::Base
-  default_scope { order('id ASC') }
-
-  attr_accessible :number, :department
-
-  belongs_to :location, touch: true
-
-  auto_strip_attributes :number, :department, squish: true
-end
