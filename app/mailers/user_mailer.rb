a56f81487935dd8564547b3fcd14c40e9c48a950
Bryan Villafañe <briananime07@gmail.com>
2019-02-18 17:54:50 -0500

    CS-616 Add email after user register

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: SETTINGS[:confirmation_email]
+
+  def new_registration(user)
+    @user = user
+    mail(to: user.email, subject: "Successful registration: Here is your new account info")
+  end
+end
