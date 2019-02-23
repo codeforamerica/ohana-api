6552cefbe0ad5e7ae2222f1bd41c812c166685f8
Bryan Villafañe <briananime07@gmail.com>
2019-02-22 20:19:34 -0500

    CS-615 Add Password update endpoint

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -13,4 +13,23 @@ class PasswordsController < Devise::PasswordsController
              status: :unprocessable_entity
     end
   end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+    else
+      set_minimum_password_length
+    end
+
+    if resource.errors.empty?
+      render json: resource, serializer: UserSerializer
+    else
+      render(
+        json: ErrorSerializer.serialize(resource.errors),
+        status: :unprocessable_entity
+      )
+    end
+  end
 end
