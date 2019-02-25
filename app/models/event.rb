e3821eecc71223a6e54f883bcb06ec9888efb614
Bryan Villafañe <briananime07@gmail.com>
2019-02-25 11:13:06 -0500

    CS-672 Add Event show view and featured action

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  MAX_EVENTS_FEATURED_MONTH = 3
   attr_accessible :title, :posted_at, :starting_at, :ending_at, :street_1,
                   :street_2, :city, :state_abbr, :zip, :phone, :external_url,
                   :organization_id, :is_featured, :body, :user_id
@@ -12,9 +13,23 @@ class Event < ActiveRecord::Base
   validates :ending_at, presence: true
   validates :street_1, presence: true
   validates :city, presence: true
+  validate :three_events_in_month?
 
-  def self.events_in_month(month)
-    where('EXTRACT(MONTH FROM starting_at) = ?', month)
+
+  def three_events_in_month?
+    featured_events_in_month = Event.events_in_month(self.starting_at)
+                                .where(is_featured: true)
+    if self.is_featured && featured_events_in_month.count >= MAX_EVENTS_FEATURED_MONTH
+      errors.add(:starting_at, I18n.t('errors.messages.three_events_in_month'))
+    end
+  end
+
+  def self.events_in_month(starting_at)
+    where(
+      'starting_at >= ? AND starting_at <= ?',
+      starting_at.beginning_of_month,
+      starting_at.end_of_month
+    )
   end
 
   def self.with_orgs(ids)
