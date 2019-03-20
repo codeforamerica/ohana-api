e2dbf6fc92b87a213ea022778010f181d2a2a01b
Bryan Villafañe <briananime07@gmail.com>
2019-03-20 11:43:00 -0500

    CS-750 Add validation for user organization not being approved yet. Refactor on devise-jwt integration. Return organization_id on successful login

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :validatable, :recoverable,
          :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
+
+
+  def active_for_authentication?
+    # Uncomment the below debug statement to view the properties of the returned self model values.
+    # logger.debug self.to_yaml
+
+    super && organization_approved?
+  end
+
+  def organization_approved?
+    organization.approval_status == 'approved'
+  end
+
+  def inactive_message
+    organization_approved? ? super : :organization_inactive
+  end
 end
