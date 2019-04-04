97ca96622529ca5710a5c0cde86d5e44cd26edfb
Bryan Villafañe <briananime07@gmail.com>
2019-04-04 15:44:49 -0500

    CS-787 Return ok status instead of delete when Deleting a user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -49,7 +49,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     resource.destroy
     render json: { message: 'User was deleted successfully' },
-           status: :destroyed
+           status: :ok
   end
 
   private
