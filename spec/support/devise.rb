a8907dd822ffd4c3fa3380458041346766d983b1
Sam Hanes <sam@fullstacklabs.co>
2019-06-06 17:22:30 -0700

    Fix import error in Devise support for RSpec.

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,5 @@
+require_relative 'admin/controller_helpers'
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
