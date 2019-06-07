e42cbcc414f0fc8e5b77b455051c8e2239c4f866
Sam Hanes <sam@fullstacklabs.co>
2019-06-06 17:16:17 -0700

    Generate unique emails for spec User factory.
    This resolves a unique constraint failure in many, many tests
    when the `:user` factory was called more than once.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :valid_user_email do |n|
+    "valid#{n}@example.com"
+  end
+
   factory :user do
     name 'Test User'
-    email 'valid@example.com'
+    email { generate :valid_user_email }
     password 'mong01dtest'
     password_confirmation 'mong01dtest'
     # required if the Devise Confirmable module is used
