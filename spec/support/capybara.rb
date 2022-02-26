2c5f3ea21c6baf70ac3ec24843b9d8cfe6096072
Moncef Belyamani <moncef@example.com>
2022-02-26 11:16:56 -0500

    Fix Selenium deprecation warning

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -8,12 +8,12 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   browser_options = Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << '--headless'
-  browser_options.args << '--disable-gpu'
+  browser_options.add_argument('--headless')
+  browser_options.add_argument('--disable-gpu')
 
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
-                                 options: browser_options
+                                 capabilities: [browser_options]
 end
 
 Capybara.javascript_driver = :headless_chrome
